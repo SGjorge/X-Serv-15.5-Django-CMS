fd74be6437111553e8462796531089def2b77361
jorge simon gil <jsimon@epsilon27.aulas.gsyc.es>
2015-03-23 20:56:06 +0100

    ejercicio resuelto

--- myproject/urls.py
+++ myproject/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'myproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^(.*)', 'cms.views.contentapp',),  
 )
